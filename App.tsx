a8b3fae0263812d01e99f1efd90f47832428d383
itsASR <68419561+itsASR@users.noreply.github.com>
2025-10-26 14:01:20 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import WebViewScreen from "./app/WebViewScreen";
+
+export default function App() {
+  return <WebViewScreen />;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Nesto - Expo SDK 53 TypeScript starter project with WebView
+
+Run:
+1. npm install
+2. npx expo start
